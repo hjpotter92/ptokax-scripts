ec01c613c5264756d45acc7413e0229cc12e6b46
hjpotter92 <hjpotter92@iitkgp.ac.in>
2013-05-04 22:00:48 +0530

    Separate Google analytics script

---
+++ javascripts/analytics.js
@@ -0,0 +1,2 @@
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript language='JavaScript' src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
