a6d9e76a40adf3f9be5ed3081735bb4ccfe38f69
Amritanshu Varshney <hjpotter92@iitkgp.ac.in>
2013-05-25 00:08:47 +0530

    Create restrictions script
     - Nick restrictions for all users
    - Mainchat restriction for unregistered users

---
+++ files/external/restrict/nicks.lua
@@ -0,0 +1,19 @@
+tSettings = {
+	sAsBot = "<"..(SetMan.GetString( 21 ) or "PtokaX").."> ",
+	sBotName = SetMan.GetString( 21 ) or "PtokaX",
+	iBanTime = 6,
+}
+
+function UserConnected( tUser )
+	if tUser.sNick:find( "^[^%w%.]" ) then
+		Core.SendToUser( tUser, tSettings.sAsBot..Error("gen", 50) )
+		BanMan.TempBan( tUser, tSettings.iBanTime, Error("gen", 50), tSettings.sBotName, true )
+		Core.Disconnect( tUser )
+	end
+	if tUser.sNick:find( "\160" ) then
+		Core.SendToUser( tUser, tSettings.sAsBot.."Certain characters are not allowed in nicks." )
+		Core.Disconnect( tUser )
+	end
+end
+
+RegConnected, OpConnected = UserConnected, UserConnected
---
+++ restrictions.lua
@@ -0,0 +1,20 @@
+function OnStartup()
+	tConfig, tList = {
+		sPath = "/root/PtokaX/scripts/",
+		sFiles = "files/external/restrict/",
+		sHubAddress = SetMan.GetString( 2 ) or "localhost",
+		sProtocol = "http://",
+	}, {
+		"chat.lua",
+		"share.lua",
+		"nicks.lua",
+	}
+	tConfig.sHubFAQ = tConfig.sProtocol..tConfig.sHubAddress.."/faq.php?code=%s&num=%04d"
+	for iIndex, sScript in ipairs( tList ) do
+		dofile( tConfig.sPath..tConfig.sFiles..sScript )
+	end
+end
+
+function Error( sCode, iNum )
+	return tConfig.sHubFAQ:format( sCode:upper(), iNum )
+end
