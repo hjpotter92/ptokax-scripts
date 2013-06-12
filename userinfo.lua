1bc15f10e6cdfa7100f7b2d702f78d1da8c20467
Amritanshu Varshney <hjpotter92@iitkgp.ac.in>
2013-06-12 17:36:12 +0530

    Finalize LogIn function

--- userinfo.lua
+++ userinfo.lua
@@ -1,6 +1,6 @@
 function OnStartup()
 	tConfig = {
-		sPath = "/root/Ptokax/scripts/files/",
+		sPath = Core.GetPtokaXPath().."scripts/files/",
 		sTextPath = "texts/",
 		sDependent = "dependency/",
 		sFunctionsFile = "users.lua",
@@ -12,54 +12,29 @@ function OnStartup()
 	}
 	dofile( tConfig.sPath..tConfig.sDependent..tConfig.sFunctionsFile )
 	tFunction.Connect()
-	tUsers = Core.GetOnlineRegs()
-	for iIndex, tUser in ipairs(tUsers) do
-		local tSend = {}
-		tSend.sDate = os.date( tConfig.sDateFormat, Core.GetUserValue(tUser, 25) )
-		tSend.sIP = tUser.sIP
-		tSend.sNick = tUser.sNick
-		tSend.iProfile = tUser.iProfile
-		if Core.GetUserAllData( tUser ) then
-			tSend.sMode = tUser.sMode or "S"
-			if tUser.sMode == "P" and not tConfig.sPassiveBanPass:find( tUser.iProfile ) then
-				BanMan.TempBan( tUser, tConfig.iPassiveBanTime, "Connection with passive mode is not allowed.", sMainBot, false )
-				Core.Disconnect( tUser )
+	local tOnlineUsers = Core.GetOnlineUsers(true)
+	for iIndex, tUser in ipairs(tOnlineUsers) do
+		repeat
+			if CheckUserMode( tUser.sNick, tUser.sMode, tUser.iProfile ) then
+				break
 			end
-			tSend.sMail = ( tUser.sEmail and ("'%s'"):format(SQLCon:escape(tUser.sEmail)) or "NULL" )
-			tSend.sDesc = ( tUser.sDescription and ("'%s'"):format(SQLCon:escape(tUser.sDescription)) or "NULL" )
-			tSend.sTag = tUser.sTag or "NULL TAG"
-			tSend.iShare = tostring(tUser.iShareSize) or 0
-			tSend.sClient = string.format( "%s %s", tUser.sClient or "N/A", tUser.sClientVersion or "N/A" )
-			tSend.iSlots = tUser.iSlots or 0
-			tSend.iHubs = tUser.iHubs or 0
-		end
+			tUser.sDate = os.date( tConfig.sDateFormat, tUser.iLoginTime )
+			tUser.iShare = tostring(tUser.iShareSize) or 0
+			tFunction.LogIn( tUser )
+		until true
 	end
+	tOnlineUsers = nil
 
 end
 
 function UserConnected( tUser )
-	local tSend = {}
-	tSend.sIP = tUser.sIP
-	tSend.iIPID = tFunction.ipstat( tSend )
-	tSend.sNick = tUser.sNick
-	tFunction.ipnstat( tSend )
-	tSend.iProfile = tUser.iProfile
-	if Core.GetUserAllData( tUser ) then
-		tSend.sMode = tUser.sMode or "S"
-		if tUser.sMode == "P" and not tProfiles.passive[tUser.iProfile] then
-			BanMan.TempBan( tUser, 15, "Connection with passive mode is not allowed.", sMainBot, false )
-			Core.Disconnect( tUser )
-		end
-		tSend.sMail = (tUser.sEmail and string.format("'%s'", SQLCon:escape(tUser.sEmail)) or "NULL")
-		tSend.sDesc = (tUser.sDescription and string.format("'%s'", SQLCon:escape(tUser.sDescription)) or "NULL")
-		tSend.sTag = tUser.sTag or "N/A"
-		tSend.iShare = tostring(tUser.iShareSize)
-		tSend.sClient = string.format( "%s %s", tUser.sClient or "N/A", tUser.sClientVersion or " N/A" )
-		tSend.iSlots = tUser.iSlots or 0
-		tSend.iHubs = tUser.iHubs or 0
-		tSend.iNickID = tFunction.nickstat( tSend )
+	Core.GetUserAllData( tUser )
+	if CheckUserMode( tUser.sNick, tUser.sMode, tUser.iProfile ) then
+		break
 	end
-	tFunction.login( tSend.iNickID, tSend.sIP, tSend.sDate )
+	tUser.sDate = os.date( tConfig.sDateFormat, tUser.iLoginTime )
+	tUser.iShare = tostring(tUser.iShareSize) or 0
+	tFunction.LogIn( tUser )
 end
 
 function UserDisconnected( tUser )
@@ -87,7 +62,7 @@ function ChatArrival( tUser, sMessage )
 end
 
 function ToArrival( tUser, sMessage )
-	local sTo = sMessage:match( "^\$To: (%S+) From" )
+	local sTo = sMessage:match( "To: (%S+) From:" )
 	if sTo ~= tConfig.sBotName or not tConfig.sAllowed:find( tUser.iProfile ) then
 		return false
 	end
@@ -102,4 +77,13 @@ function ToArrival( tUser, sMessage )
 		tCommands.ii( sData, tUser.sNick )
 		return true
 	end
+end
+
+local function CheckUserMode( sNick, sMode, iProfile )
+	if sMode == "P" and not tConfig.sPassiveBanPass:find( iProfile ) then
+		BanMan.TempBanNick( sNick, tConfig.iPassiveBanTime, "Connection with passive mode is not allowed.", tConfig.sBotName, false )
+		Core.Disconnect( sNick )
+		return true
+	end
+	return false
 end
