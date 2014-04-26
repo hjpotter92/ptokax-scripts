19caef07e8755045ae079b3d3d9665ceae23d7e5
Amritanshu Varshney <hjpotter92@iitkgp.ac.in>
2014-04-26 17:44:04 +0530

    Fix registration process
    Scopes of various variables was undefined

--- registration.lua
+++ registration.lua
@@ -16,19 +16,18 @@ function OnStartup()
 		sPrefix = "[-+/?#!]",
 		tTemplates = {
 			sRegSuccess = "\n\n\tYou have successfully registered yourself.\n\t"..
-				string.rep( "¯",40 ).."\n"..
+				string.rep( "Â¯",40 ).."\n"..
 				"\tUser Name:\t %-32s \n"..
 				"\tPassword:\t %s \n\t"..
 				"\tProfile:\t %s \n\t"..
 				string.rep( "_",40 ).."\n"..
 				"\tPlease make a note of this information.\n\tPlease reconnect to activate your status.\n",
 			sPassChange = "\n\n\tYou've changed your password successfully\n\t"..
-				"from %s to %s . Update your favorite hub\n\t"
+				"from %s to %s . Update your favorite hub\n\t"..
 				"settings to use the new password.",
-			sHelpMenu = "\n\n\tRegMe Command Help\n\n\tCommand\t\tDescription\n\t"..string.rep( "¯",40 ).."\n"
+			sHelpMenu = "\n\n\tRegMe Command Help\n\n\tCommand\t\tDescription\n\t"..string.rep( "Â¯",40 ).."\n"
 		}
 	}
-	tRegCmds = {}
 end
 
 function ToArrival( tUser, sData )
@@ -53,82 +52,81 @@ function PasswordCheck( sPassword )
 	end
 	return false
 end
-
-function tRegCmds.regme( tUser, sData, sCmd )
-	if bRegFlag then
-		if tUser then
-			local sNick = tUser.sNick
-			if tUser.iProfile ~= -1 then
-				return "Don't be silly "..sNick..". you're already registered here."
-			else
-				local sPassword = sData:find("%b<> .%w+ (%S+)|$")
-				if not sPassword then
-					return "Error! Usage: "..tConfig.sPrefix..sCmd.." <password>"
-				end
-				if PasswordCheck(sPassword) then
-					return "Your password contains invalid characters. Please choose a new one."
-				end
-				RegMan.AddReg( sNick, sPassword, iRegProfile )
-				RegMan.Save()
-				return tConfig.tTemplates.sRegSuccess:format( sNick, sPassword, tConfig.sRegProfileName )
-			end
-		else
-			return "Register yourself"
-		end
-	else
-		return "Registrations not open yet"
-	end
-end
-
-function tRegCmds.passwd( tUser, sData, sCmd )
-	if tUser then
-		if tUser.iProfile == -1 then
-			return "Don't be silly "..tUser.sNick.." you're not registered here."
-		else
-			local sOldPass, sNewPass = sData:match("%b<> .%w+ (%S+) (%S+)|$")
-			if sOldPass and sNewPass then
-				local sPassword, iProfile = RegMan.GetReg( tUser.sNick ).sPassword, tUser.iProfile
-				if sPassword and iProfile then
-					if sOldPass:lower() ~= sPassword:lower() then
-						return "That is not your correct password. Please try again. [case insensitive]"
+tRegCmds = {
+	regme = function( tUser, sData, sCmd )
+		if tConfig.bRegFlag then
+			if tUser then
+				local sNick = tUser.sNick
+				if tUser.iProfile ~= -1 then
+					return "Don't be silly "..sNick..". you're already registered here."
+				else
+					local sPassword = sData:find("%b<> .%w+ (%S+)|$")
+					if not sPassword then
+						return "Error! Usage: "..tConfig.sPrefix..sCmd.." <password>"
 					end
-					if PasswordCheck( sNewPass ) then
+					if PasswordCheck(sPassword) then
 						return "Your password contains invalid characters. Please choose a new one."
 					end
-					if sNewPass == sOldPass then
-						return "Your cannot change to the same password. Please choose a different one."
-					end
-					RegMan.ChangeReg( tUser.sNick, sNewPass, iProfile )
-					RegMan.Save()
-					return tConfig.tTemplates.sPassChange:format( sOldPass, sNewPass )
+					RegMan.AddReg( sNick, sPassword, iRegProfile )
+					return tConfig.tTemplates.sRegSuccess:format( sNick, sPassword, tConfig.sRegProfileName )
 				end
 			else
-				return "Error! Usage: ."..tConfig.sPrefix..sCmd.." <old password> <new password>"
+				return "Register yourself"
 			end
+		else
+			return "Registrations not open yet"
 		end
-	else
-		return "Change your password"
-	end
-end
+	end,
 
-function tRegCmds.rmhelp( tUser, sData, sCmd )
-	if tUser then
-		local sReply = tConfig.tTemplates.sHelpMenu
-		for i, v in pairs(tRegCmds) do
-			local sDescription = tRegCmds[i]()
-			sReply = sReply.."\t!"..("%-15s"):format( i ).."\t"..sDescription.."\n"
+	passwd = function( tUser, sData, sCmd )
+		if tUser then
+			if tUser.iProfile == -1 then
+				return "Don't be silly "..tUser.sNick.." you're not registered here."
+			else
+				local sOldPass, sNewPass = sData:match("%b<> .%w+ (%S+) (%S+)|$")
+				if sOldPass and sNewPass then
+					local sPassword, iProfile = RegMan.GetReg( tUser.sNick ).sPassword, tUser.iProfile
+					if sPassword and iProfile then
+						if sOldPass:lower() ~= sPassword:lower() then
+							return "That is not your correct password. Please try again. [case insensitive]"
+						end
+						if PasswordCheck( sNewPass ) then
+							return "Your password contains invalid characters. Please choose a new one."
+						end
+						if sNewPass == sOldPass then
+							return "Your cannot change to the same password. Please choose a different one."
+						end
+						RegMan.ChangeReg( tUser.sNick, sNewPass, iProfile )
+						return tConfig.tTemplates.sPassChange:format( sOldPass, sNewPass )
+					end
+				else
+					return "Error! Usage: ."..tConfig.sPrefix..sCmd.." <old password> <new password>"
+				end
+			end
+		else
+			return "Change your password"
 		end
-		return sReply.."\n\t"..string.rep("¯",40).."\n\n"
-	else
-		return "Registration help menu","",""
-	end
-end
+	end,
 
-function tRegCmds.regflag( tUser, sData, sCmd )
-	local sReturn = "The registration flag is set to: "..tostring(tConfig.bRegFlag)
-	if tUser.iProfile == 0 then
-		tConfig.bRegFlag = not bRegFlag
-		sReturn = "Registration flag changed to: "..tostring(tConfig.bRegFlag)
-	end
-	return sReturn
-end
+	rmhelp = function( tUser, sData, sCmd )
+		if tUser then
+			local sReply = tConfig.tTemplates.sHelpMenu
+			for i, v in pairs(tRegCmds) do
+				local sDescription = tRegCmds[i]()
+				sReply = sReply.."\t!"..("%-15s"):format( i ).."\t"..sDescription.."\n"
+			end
+			return sReply.."\n\t"..string.rep("Â¯",40).."\n\n"
+		else
+			return "Registration help menu","",""
+		end
+	end,
+
+	regflag = function( tUser, sData, sCmd )
+		local sReturn = "The registration flag is set to: "..tostring(tConfig.bRegFlag)
+		if tUser.iProfile == 0 then
+			tConfig.bRegFlag = not tConfig.bRegFlag
+			sReturn = "Registration flag changed to: "..tostring(tConfig.bRegFlag)
+		end
+		return sReturn
+	end,
+}
