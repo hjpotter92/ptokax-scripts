cf50fa9dd3a2081f44c6cd653ab3a6eb8d52934a
Amritanshu Varshney <hjpotter92@iitkgp.ac.in>
2014-06-04 23:27:01 +0530

    Cleaner eat-digest-expel setup
     - Remove terminating spaces
     - Use global variable for bot name wherever applicable

--- files/modifiers.lua
+++ files/modifiers.lua
@@ -7,36 +7,34 @@
 
 --]]
 
-
-modifiers=function(user,data)	
-	if isthere(user.sNick,muted) then 
+modifiers = function(user, data)
+	if isthere(user.sNick, muted) then
 		if user.sNick ~= "IRC" then
-			Core.SendToUser(user,"<PtokaX> You are muted.|")
+			Core.SendToUser(user, "<PtokaX> You are muted.|")
 		end
 		return false
 	end
-	 if isthere_key(user.sNick,unsubbed) then
-                Core.SendToUser(user,"<PtokaX> You have unsubscribed from Mainchat.To subscribe again enter !sub|")
+	 if isthere_key(user.sNick, unsubbed) then
+		Core.SendToUser(user, "<PtokaX> You have unsubscribed from mainchat. To subscribe again enter !sub|")
 		return false
 	end
-	local msg = string.gsub(data,"%b<>(.*)","%1")
-	local nick=user.sNick
-	if isthere(user.sNick,nickc) then 
+	local msg, nick = data:match "%b<>(.*)", user.sNick
+	if isthere(user.sNick, nickc) then
 		nick = nickc[user.sNick]
 	end
 	if desu then
-	   msg = msg.." desu"
-        end
+		msg = msg.." desu"
+	end
 	--allow only 1 at a time
 	if san then
-		nick=nick.."-san"
-        end
-	if chan then
-		nick=nick.."-chan"
+		nick = nick.."-san"
 	end
-	local finalmsg="<"..nick..">"..msg
-	if isthere(user.sNick,falone) then 
-		Core.SendToUser(user,finalmsg)
+	if chan then
+		nick = nick.."-chan"
+	end
+	local finalmsg = "<"..nick..">"..msg
+	if isthere(user.sNick, falone) then
+		Core.SendToUser(user, finalmsg)
 		return false
 	end
 	return finalmsg
